3a9cbcf97a5fc2cbc15db27b60187ea99cf11b17
Arian Ghasempour <arianghsm721@gmail.com>
2026-04-20 18:35:12 +0330

    Nothing

--- main.py
+++ main.py
@@ -1,34 +1,49 @@
+
 import asyncio
-import telegram
+from telegram import Bot, Update, InlineKeyboardButton, CopyTextButton, InlineKeyboardMarkup
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 from config import bot_token
 import logging
-from telegram import Update
-from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler
 
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE, user=None):
+    user_id = update.effective_user.id
+    user_name = update.effective_user.name
+
+    keyboard = [
+        [
+            InlineKeyboardButton(text="کپی کردن آیدی عددی من",copy_text=CopyTextButton(text=str(user_id))),
+            InlineKeyboardButton(text='نمایش مشخصات شما', callback_data=user_information),
+        ]
+    ]
+    reply_markup = InlineKeyboardMarkup(keyboard)
+
+    await update.effective_message.reply_text(
+        text=f'سلام آقای {user_name}، به ربات خوش اومدی.',
+        reply_markup=reply_markup
+    )
+
+async def user_information(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    query = update.callback_query
+    await query.edit_message_text(text="مشخصات حساب کاربری شما:", reply_markup=InlineKeyboardMarkup())
+    keyboard = InlineKeyboardMarkup([
+        [
+            InlineKeyboardButton(text='بروزرسانی', callback_data=user_information),
+         InlineKeyboardButton(text='بازگشت', callback_data=start)
+            ],
+        [InlineKeyboardButton(text=f'نام حساب کاربری: {update.effective_user.first_name}', callback_data=start)]
+    ])
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
     level=logging.INFO
 )
 
-
-async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await context.bot.send_message(
-        chat_id=update.effective_chat.id,
-        text="I'm a bot, please talk to me!")
-
-
-if __name__ == '__main__':
-    application = ApplicationBuilder().token(bot_token).build()
-
-    start_handler = CommandHandler('start', start)
-    application.add_handler(start_handler)
-
-    application.run_polling()
-
-async def main():
-    bot = telegram.Bot(token=bot_token)
+async def bot_turnOn():
+    bot = Bot(token=bot_token)
     async with bot:
         print(await bot.get_me())
+    application = ApplicationBuilder().token(bot_token).build()
+    application.add_handler(CommandHandler('start', start()))
+    application.run_polling()
 
 if __name__ == '__main__':
-    asyncio.run(main())
+    asyncio.run(bot_turnOn())
